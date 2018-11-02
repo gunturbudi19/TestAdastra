e62279ccf35cb08922854c18374eb7f1eff662e8
Guntur Budi <guntur.budi19@gmail.com>
2018-11-02 10:09:47 +0700

    Class Declaration

---
+++ TestAdastra/Helper/GlobalMethod.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalMethod.swift
+//  TestAdastra
+//
+//  Created by Guntur Budi on 02/11/18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class GlobalMethod: NSObject {
+
+}
---
+++ TestAdastra/Helper/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TestAdastra
+//
+//  Created by Guntur Budi on 02/11/18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
